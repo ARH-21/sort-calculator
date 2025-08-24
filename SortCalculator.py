9e844b3ad40dbaffd76cc994c16d6378538279a7
ARH-21 <armheyd21@gmail.com>
2025-08-24 16:55:35 -0700

    Started project with base idea

---
+++ SortCalculator.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+import time 
+
+
+# This is a "Sorting Calculator". It is inspired by Sort Detective Assignment I did.
+# The difference here is the user will know what Sorting Algorithm will be used when the button is pressed.
+
+# To-Do (GUI):
+# BUTTONS:
+# 6 buttons for each Alg
+# A button to Load File
+# A button to clear the data (table)
+
+# TABLE:
+# The table should show:
+#  Trial | File | Alg | ms | Moves | Comparisons
+#   1.   | test.txt | BubbleSort | 0ms | 236 | 67
+
+# Text Listed
+# Total integers in File: 100
+# The (current) filename: mydata.txt
+
+
+
+# file directory
+
+
+# Bubble Sort
+
+
+# Selection Sort
+
+
+# Insertion Sort
+
+
+# Merge Sort
+
+
+# Quicksort
+
+
+# Heapsort
+
