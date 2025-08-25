b3da4243d457a9df5ac2e7ff0224755143d4b078
Armon H. <armheyd21@gmail.com>
2025-08-24 23:23:37 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
-# This is a "Sorting Calculator". It is inspired by Sort Detective Assignment I did.
-# The difference here is the user will know what Sorting Algorithm will be used when the button is pressed.
+# CREDITS:
+# Sorting Algorithms adapted from GeeksforGeeks Python examples
+# Any logic or psuedocode was referenced by 'C++ Data Structures' (Dale, Weems, Richards, 6th Edition)
+# Project concept inspired by Dr. David B. Levine's Sort Detective
+
+from sort_gui import SortGUI
+from tkinter import Tk
+
+if __name__ == "__main__":
+    root = Tk()
+    app = SortGUI(root)
+    root.mainloop()
