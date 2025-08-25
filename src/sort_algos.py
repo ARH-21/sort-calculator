30fd33952c65ef80888ddfc2491ac7fea46dd30a
ARH-21 <armheyd21@gmail.com>
2025-08-24 17:48:59 -0700

    Implemented bubble insertion algs

--- src/sort_algos.py
+++ src/sort_algos.py
@@ -1,4 +1,13 @@
+# read text file and sort
+# copy() save original unsorted data for each press
+
+
 # Bubble Sort
+def bubble_sort(arr):
+
+# if 5 > 8 no swap
+# if 8 > 5 swap
+
 
 
 # Selection Sort
